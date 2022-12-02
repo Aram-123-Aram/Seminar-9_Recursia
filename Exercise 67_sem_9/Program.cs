f85aaa14e46f466c1d0893ea394e00aa3526b818
Aram <armsir-8182@mail.ru>
2022-12-02 18:20:00 +0300

    Finding summa of numbers in asks number

---
+++ Exercise 67_sem_9/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 67. Напишите программу,который на вход будет принимать число,
+и возвращать сумму его цифр. */
+
+Console.Write("Enter the number n= ");
+int.TryParse(Console.ReadLine()!, out int n);
+
+/* int SumNumbers(int m, int sum = 0)
+{
+    if(m/10 == 0) {return sum = sum + m%10;}
+    else {return SumNumbers(m/10, sum = sum + m%10); }
+}
+Console.Write(SumNumbers(n)); */
+
+void SumNumbers(int m, int sum = 0)
+{
+    if(m/10 == 0) {Console.Write(sum=sum+m%10); }
+    else SumNumbers(m/10, sum = sum +m%10);
+}
+SumNumbers(n);
