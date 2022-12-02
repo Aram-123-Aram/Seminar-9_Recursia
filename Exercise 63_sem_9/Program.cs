8f2ab940e7296f1def36b766ee701c4bd2209a2b
Aram <armsir-8182@mail.ru>
2022-12-02 17:39:54 +0300

    Added 3 various variants solving exercise 63

---
+++ Exercise 63_sem_9/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 63. Задайте значение N. Написать программу,который выведет все натуральные число от 1 до N. */
+
+Console.Write("Enter the N= ");
+int.TryParse(Console.ReadLine()!, out int N);
+
+/*int Numbers(int n, int k=1)
+{
+    if( k == n) return n;
+    else Console.Write(k); return Numbers(n, k+1);
+}
+
+Console.Write(Numbers(N)); */
+
+/* void Numbers(int n, int k=1)
+{
+    if(k == n) {Console.Write(k); }
+    else {Console.Write(k);
+    Numbers(n, k+1); }
+}
+Numbers(N); */
+
+string Numbers(int n, int k=1)
+{
+    if(k==n) {return $"{k}"; }
+    else return $"{k} {Numbers(n, k+1)}";
+}
+Console.Write(Numbers(N)); */
+
