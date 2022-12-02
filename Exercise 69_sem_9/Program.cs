db088f3b6cc3e8ef0682ea1c656cbbb418814f10
Aram <armsir-8182@mail.ru>
2022-12-02 20:59:52 +0300

    Finding the result of A^B

---
+++ Exercise 69_sem_9/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 69. Написать программу,который на вход принимает два числв A и B и 
+возводит число А в целую степень В с помощью рекурсии.
+Например:
+A=3, B=5 -> 243(3^5) 3*3^4 */
+
+Console.Write("Enter the number A= ");
+int.TryParse(Console.ReadLine()!, out int A);
+Console.Write("Enter the number B= ");
+int.TryParse(Console.ReadLine()!, out int B);
+
+int NumberAB(int a, int b)
+{
+    if(b == 1) return a;
+    else return a = a*NumberAB(a, b-1);
+}
+Console.Write(NumberAB(A, B));
