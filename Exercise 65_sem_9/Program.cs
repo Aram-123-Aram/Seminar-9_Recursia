20e329ef994b68e802881130823de6600279102d
Aram <armsir-8182@mail.ru>
2022-12-02 17:41:36 +0300

    solving the same exercise from m to n

---
+++ Exercise 65_sem_9/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 63. Задайте значение M и N. Написать программу,
+который выведет все натуральные число от M до N. */
+
+Console.Write("Enter the M= ");
+int.TryParse(Console.ReadLine()!, out int M);
+Console.Write("Enter the N= ");
+int.TryParse(Console.ReadLine()!, out int N);
+
+void ShowNumbers(int m, int n)
+{
+    if(m == n) Console.Write($"{m}");
+    else {Console.Write($"{m} "); ShowNumbers(m+1, n); }
+
+}
+Console.Write("-> ");
+ShowNumbers(M, N);
